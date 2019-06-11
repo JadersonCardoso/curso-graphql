70a7fb5f8d3aa10cb3b843de1c7eda713fed9b38
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-06-11 00:46:05 -0300

    Aula exemplo da primeira query

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const {ApolloServer, gql } = require('apollo-server')
 
 const typeDefs = gql`
-
+    # Pontos de entrada da sua API!
+    type Query {
+        ola: String
+    }
 `
 const resolvers = {
-
+    Query: {
+        ola() {
+            return 'Primeira Query com retorno de uma String!'
+        }
+    }
 }
 
 const server = new ApolloServer ({
