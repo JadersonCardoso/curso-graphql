b476e6180f9be1ff3b748d0486a72ca1b91a7d68
Jaderson <jaderson.brandao@gmail.com>
2019-06-26 16:47:46 -0300

    Desafio Hora Atual

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const typeDefs = gql`
     # Pontos de entrada da sua API!
     type Query {
         ola: String
+        horaCerta: String
     }
 `
 const resolvers = {
     Query: {
         ola() {
             return 'Primeira Query com retorno de uma String!'
-        }
+        },
+        horaCerta() {
+            return `${new Date}`
+        }       
     }
 }
 
