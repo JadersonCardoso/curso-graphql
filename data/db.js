f2353f80a49eb2d7c42057df0bc70dc6f2453996
Jaderson Brandão <jaderson.brandao@gmail.com>
2019-06-29 01:31:10 -0300

    utulizando enum para status do usuário

--- data/db.js
+++ data/db.js
@@ -11,19 +11,22 @@ const usuarios = [{
     nome: 'João Silva',
     email: 'jsilva@zemail.com',
     idade: 29,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'ATIVO'
 },{
     id: 2,
     nome: 'Rafael Junior',
     email: 'rafajun@wemail.com',
     idade: 31,
-    perfil_id: 2
+    perfil_id: 2,
+    status: 'INATIVO'
 },{
     id: 3,
     nome: 'Daniela Smith',
     email: 'danismi@umail.com',
     idade: 24,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'BLOQUEADO'
 }]
 
 module.exports = { usuarios, perfis}
